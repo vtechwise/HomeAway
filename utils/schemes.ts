59474005cfeb9d1ceac98a638f3e16135891cf4d
victor <oguntayovictor65@gmail.com>
2025-06-23 00:09:24 +0100

    work on createProfileAction setup

--- utils/schemes.ts
+++ utils/schemes.ts
@@ -3,5 +3,5 @@ import * as z from "zod";
 export const profileScheme = z.object({
   firstName: z.string().min(2),
   lastName: z.string().min(2),
-  userName: z.string().min(2),
+  username: z.string().min(2),
 });
